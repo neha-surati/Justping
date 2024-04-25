0309f00f5ebedab357062c4a357b284d8894ffca
Neha <nehasurati1605@gmail.com>
2024-04-25 18:27:22 +0530

    fix some bugs

--- header.php
+++ header.php
@@ -269,58 +269,8 @@ if (!isset($_SESSION["userlogin"])) {
                                 </div>
                             </a>
                         </li>
-                        <li class="menu nav-item">
-                            <a href="services.php"
-                                class="nav-link group <?php echo basename($_SERVER["PHP_SELF"]) == "services.php" ? "active" : "" ?>">
-                                <div class="flex items-center">
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
-                                        <path opacity="0.5"
-                                            d="M2 17C2 15.1144 2 14.1716 2.58579 13.5858C3.17157 13 4.11438 13 6 13H18C19.8856 13 20.8284 13 21.4142 13.5858C22 14.1716 22 15.1144 22 17C22 18.8856 22 19.8284 21.4142 20.4142C20.8284 21 19.8856 21 18 21H6C4.11438 21 3.17157 21 2.58579 20.4142C2 19.8284 2 18.8856 2 17Z"
-                                            stroke="currentColor" stroke-width="1.5"></path>
-                                        <path opacity="0.5"
-                                            d="M2 6C2 4.11438 2 3.17157 2.58579 2.58579C3.17157 2 4.11438 2 6 2H18C19.8856 2 20.8284 2 21.4142 2.58579C22 3.17157 22 4.11438 22 6C22 7.88562 22 8.82843 21.4142 9.41421C20.8284 10 19.8856 10 18 10H6C4.11438 10 3.17157 10 2.58579 9.41421C2 8.82843 2 7.88562 2 6Z"
-                                            stroke="currentColor" stroke-width="1.5"></path>
-                                        <path d="M11 6H18" stroke="currentColor" stroke-width="1.5"
-                                            stroke-linecap="round"></path>
-                                        <path d="M6 6H8" stroke="currentColor" stroke-width="1.5"
-                                            stroke-linecap="round"></path>
-                                        <path d="M11 17H18" stroke="currentColor" stroke-width="1.5"
-                                            stroke-linecap="round"></path>
-                                        <path d="M6 17H8" stroke="currentColor" stroke-width="1.5"
-                                            stroke-linecap="round"></path>
-                                    </svg>
-                                    <span
-                                        class="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">Services
-                                    </span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="menu nav-item">
-                            <a href="blog.php"
-                                class="nav-link group <?php echo basename($_SERVER["PHP_SELF"]) == "blog.php" ? "active" : "" ?>">
-                                <div class="flex items-center">
-                                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
-                                        <path
-                                            d="M18.18 8.03933L18.6435 7.57589C19.4113 6.80804 20.6563 6.80804 21.4241 7.57589C22.192 8.34374 22.192 9.58868 21.4241 10.3565L20.9607 10.82M18.18 8.03933C18.18 8.03933 18.238 9.02414 19.1069 9.89309C19.9759 10.762 20.9607 10.82 20.9607 10.82M18.18 8.03933L13.9194 12.2999C13.6308 12.5885 13.4865 12.7328 13.3624 12.8919C13.2161 13.0796 13.0906 13.2827 12.9882 13.4975C12.9014 13.6797 12.8368 13.8732 12.7078 14.2604L12.2946 15.5L12.1609 15.901M20.9607 10.82L16.7001 15.0806C16.4115 15.3692 16.2672 15.5135 16.1081 15.6376C15.9204 15.7839 15.7173 15.9094 15.5025 16.0118C15.3203 16.0986 15.1268 16.1632 14.7396 16.2922L13.5 16.7054L13.099 16.8391M13.099 16.8391L12.6979 16.9728C12.5074 17.0363 12.2973 16.9867 12.1553 16.8447C12.0133 16.7027 11.9637 16.4926 12.0272 16.3021L12.1609 15.901M13.099 16.8391L12.1609 15.901"
-                                            stroke="currentColor" stroke-width="1.5"></path>
-                                        <path d="M8 13H10.5" stroke="currentColor" stroke-width="1.5"
-                                            stroke-linecap="round"></path>
-                                        <path d="M8 9H14.5" stroke="currentColor" stroke-width="1.5"
-                                            stroke-linecap="round"></path>
-                                        <path d="M8 17H9.5" stroke="currentColor" stroke-width="1.5"
-                                            stroke-linecap="round"></path>
-                                        <path opacity="0.5"
-                                            d="M3 10C3 6.22876 3 4.34315 4.17157 3.17157C5.34315 2 7.22876 2 11 2H13C16.7712 2 18.6569 2 19.8284 3.17157C21 4.34315 21 6.22876 21 10V14C21 17.7712 21 19.6569 19.8284 20.8284C18.6569 22 16.7712 22 13 22H11C7.22876 22 5.34315 22 4.17157 20.8284C3 19.6569 3 17.7712 3 14V10Z"
-                                            stroke="currentColor" stroke-width="1.5"></path>
-                                    </svg>
-                                    <span
-                                        class="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">Blog
-                                    </span>
-                                </div>
-                            </a>
-                        </li>
+                        
+                    
                         <li class="menu nav-item">
                             <a href="faq.php"
                                 class="nav-link group <?php echo basename($_SERVER["PHP_SELF"]) == "faq.php" ? "active" : "" ?>">
