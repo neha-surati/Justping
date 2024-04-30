20ba993d435ecf2c39130c8acadb551af613c17e
roshnirana-2 <roshnirana0369@gmail.com>
2024-04-30 18:15:42 +0530

    add ajax valiodation in product category

--- add_product_category.php
+++ add_product_category.php
@@ -100,31 +100,44 @@ if (isset($_REQUEST["update"])) {
                 <i class="ri-arrow-left-line"></i>
             </a>
         </span>
-        <h1 class="dark:text-white-dar text-2xl font-bold">Product Category - <?php echo (isset($mode)) ? (($mode == 'view') ? 'View' : 'Edit') : 'Add' ?>
+        <h1 class="dark:text-white-dar text-2xl font-bold">Product Category -
+            <?php echo (isset($mode)) ? (($mode == 'view') ? 'View' : 'Edit') : 'Add' ?>
         </h1>
     </div>
     <div class="panel mt-6">
         <div class="mb-5">
             <form class="space-y-5" method="post">
-                
-                    <div>
-                        <label for="name">Name</label>
-                        <input id="name" name="name" type="text" class="form-input" placeholder="Enter name" value="<?php echo (isset($mode)) ? $data['name'] : '' ?>" required <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> />
-                    </div>
-                    <div>
-                        <label for="details">Details</label>
-                        <input id="details" name="details" type="text" class="form-input" placeholder="Enter detail" value="<?php echo (isset($mode)) ? $data['details'] : '' ?>" required <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> />
-                    </div>
-              
-                
+
+                <div>
+                    <label for="name">Name</label>
+                    <input type="hidden" id="pid" value="<?php echo isset($mode) ? $data['id'] : '' ?>">
+                    <input id="name" name="name" type="text" class="form-input" placeholder="Enter name"
+                    onblur="checkName(this, <?php echo isset($mode) ? $data['id'] : -1 ?>)"
+                        value="<?php echo (isset($mode)) ? $data['name'] : '' ?>" required
+                        <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> />
+                        <p class="mt-3 text-danger text-base font-bold" id="demo"></p>
+                </div>
+                <div>
+                    <label for="details">Details</label>
+                    <input id="details" name="details" type="text" class="form-input" placeholder="Enter detail"
+                        value="<?php echo (isset($mode)) ? $data['details'] : '' ?>" required
+                        <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> />
+                </div>
+
+
                 <div class="mb-4">
                     <label for="custom_switch_checkbox1">Status</label>
                     <label class="w-12 h-6 relative">
-                        <input type="checkbox" class="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer" id="status" name="status" <?php echo (isset($mode) && $data['stats'] == 'Enable') ? 'checked' : '' ?> <?php echo (isset($mode) && $mode == 'view') ? 'disabled' : '' ?>><span class="bg-[#ebedf2] dark:bg-dark block h-full rounded-full before:absolute before:left-1 before:bg-white dark:before:bg-white-dark dark:peer-checked:before:bg-white before:bottom-1 before:w-4 before:h-4 before:rounded-full peer-checked:before:left-7 peer-checked:bg-primary before:transition-all before:duration-300"></span>
+                        <input type="checkbox"
+                            class="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer" id="status"
+                            name="status" <?php echo (isset($mode) && $data['stats'] == 'Enable') ? 'checked' : '' ?>
+                            <?php echo (isset($mode) && $mode == 'view') ? 'disabled' : '' ?>><span
+                            class="bg-[#ebedf2] dark:bg-dark block h-full rounded-full before:absolute before:left-1 before:bg-white dark:before:bg-white-dark dark:peer-checked:before:bg-white before:bottom-1 before:w-4 before:h-4 before:rounded-full peer-checked:before:left-7 peer-checked:bg-primary before:transition-all before:duration-300"></span>
                     </label>
                 </div>
                 <div class="relative inline-flex align-middle gap-3 mt-4 ">
-                    <button type="submit" name="<?php echo isset($mode) && $mode == 'edit' ? 'update' : 'save' ?>" id="save" class="btn btn-success <?php echo isset($mode) && $mode == 'view' ? 'hidden' : '' ?>">
+                    <button type="submit" name="<?php echo isset($mode) && $mode == 'edit' ? 'update' : 'save' ?>"
+                        id="save" class="btn btn-success <?php echo isset($mode) && $mode == 'view' ? 'hidden' : '' ?>">
                         <?php echo isset($mode) && $mode == 'edit' ? 'Update' : 'Save' ?>
                     </button>
                     <button type="button" class="btn btn-danger" onclick="javascript:go_back()">Close</button>
@@ -135,13 +148,55 @@ if (isset($_REQUEST["update"])) {
 </div>
 
 <script type="text/javascript">
-    checkCookies();
+checkCookies();
 
-    function go_back() {
-        eraseCookie("edit_id");
-        eraseCookie("view_id");
-        window.location = "product_category_details.php";
+function go_back() {
+    eraseCookie("edit_id");
+    eraseCookie("view_id");
+    window.location = "product_category_details.php";
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const submitButton = document.getElementById('save');
+    const form = document.getElementById('mainForm');
+
+    submitButton.addEventListener('click', function() {
+        const c1 = document.getElementById("name");
+        const id = document.getElementById("pid");
+        if (!checkName(c1, id)) {
+            return false;
+        }
+    });
+});
+
+function checkName(c1, id) {
+    const n = c1.value;
+    const pid = id.value;
+
+    const obj = new XMLHttpRequest();
+    obj.open("GET", "./ajax/check_product.php?name=" + n + "&pid=" + pid, false); // synchronous request
+    obj.send();
+
+    if (obj.status == 200) {
+        const x = obj.responseText;
+        if (x >= 1) {
+            c1.value = "";
+            c1.focus();
+            document.getElementById("demo").innerHTML = "Sorry the product already exists!";
+            document.getElementById("demo").classList.remove("hidden");
+            return false;
+        } else {
+            document.getElementById("demo").innerHTML = "";
+            document.getElementById("demo").classList.add("hidden");
+            return true;
+        }
+    } else {
+        // Handle errors
+        return false;
     }
+}
 </script>
 
 <?php
--- ajax/check_product.php
+++ ajax/check_product.php
@@ -2,7 +2,7 @@
 	include "../db_connect.php";
 	$obj = new DB_Connect();
     $name = $_REQUEST["name"];
-    $id=$_REQUEST["pid"];
+    $id = $_REQUEST["pid"];
     $stmt = $obj->con1->prepare("SELECT count(*) as tot FROM `product_category` WHERE name=? AND id!=?");
     $stmt->bind_param("si", $name,$id);
     $stmt->execute();
