5a30bcebdd61a66e6f9eb10cf86beeadc3c39d41
Neha <nehasurati1605@gmail.com>
2024-04-30 18:43:47 +0530

    fix bug

--- notification.php
+++ notification.php
@@ -1,8 +1,6 @@
 <?php
 //CREATED BY HARSH (09/02/2024)
 include "header.php";
-
-
 if (isset($_REQUEST["flg"]) && $_REQUEST["flg"] == "del") {
     try {
         $n_id = $_REQUEST["n_id"];
@@ -18,6 +16,11 @@ if (isset($_REQUEST["flg"]) && $_REQUEST["flg"] == "del") {
     } catch (\Exception $e) {
         setcookie("sql_error", urlencode($e->getMessage()), time() + 3600, "/");
     }
+    
+  if ($Resp) {
+    setcookie("msg", "data_del", time() + 3600, "/");
+  } 
+  header("location:notification.php");
 }
 ?>
 
@@ -32,32 +35,31 @@ if (isset($_REQUEST["flg"]) && $_REQUEST["flg"] == "del") {
 
 <!-- script -->
 <script type="text/javascript">
-     checkCookies();
-          eraseCookie("edit_id");
-          eraseCookie("view_id");
-    checkCookies();
+checkCookies();
+eraseCookie("edit_id");
+eraseCookie("view_id");
 
-    function getActions(id) {
-        return `<ul class="flex items-center gap-4">
-       
-        </li>
+function getActions(id) {
+    return `<ul class="flex items-center gap-4">
         <li>
-            <a href="javascript:showAlert(`+id+`);" class='text-xl' x-tooltip="Delete">
+            <a href="javascript:showAlert(` + id + `);" class='text-xl' x-tooltip="Delete">
                 <i class="ri-delete-bin-line text-danger"></i>
             </a>
         </li>
     </ul>`
-    }
+}
 
-    document.addEventListener('alpine:init', () => {
-        Alpine.data('pagination', () => ({
-            // datatable: null,
-            init() {
-                this.datatable = new simpleDatatables.DataTable('#myTable', {
-                    data: {
-                        headings: ['Sr.No.', 'Type', 'Message', 'Product', 'Date-Time', 'Actions'],
-                        data: [
-                            <?php
+document.addEventListener('alpine:init', () => {
+    Alpine.data('pagination', () => ({
+        // datatable: null,
+        init() {
+            this.datatable = new simpleDatatables.DataTable('#myTable', {
+                data: {
+                    headings: ['Sr.No.', 'Type', 'Message', 'Product', 'Date-Time',
+                        'Actions'
+                    ],
+                    data: [
+                        <?php
                             $stmt = $obj->con1->prepare(
                                 "SELECT n1.*,p1.name FROM `notification_center`as n1,`product_category`as p1 WHERE n1.item_id=p1.id;"
                             );
@@ -65,79 +67,78 @@ if (isset($_REQUEST["flg"]) && $_REQUEST["flg"] == "del") {
                             $Resp = $stmt->get_result();
                             $i = 1;
                             while ($row = mysqli_fetch_array($Resp)) {
-                            ?>
-                                [
-                                    <?php echo $i; ?>,
-                                    '<?php echo $row["notification_type"]; ?>',
-                                    '<?php echo $row["msg"]; ?>',
-                                    '<?php echo $row["name"]; ?>',
-                                    '<?php echo $row["date_time"]; ?>',
-                                    getActions(<?php echo $row["id"]; ?>)
-                                ],
-                            <?php
+                            ?>[
+                            <?php echo $i; ?>,
+                            '<?php echo $row["notification_type"]; ?>',
+                            '<?php echo $row["msg"]; ?>',
+                            '<?php echo $row["name"]; ?>',
+                            '<?php echo $row["date_time"]; ?>',
+                            getActions(<?php echo $row["id"]; ?>)
+                        ],
+                        <?php
                                 $i++;
                             }
                             ?>
-                        ],
-                    },
-                    perPage: 10,
-                    perPageSelect: [10, 20, 30, 50, 100],
-                    columns: [{
-                        select: 0,
-                        sort: 'asc',
-                    }, ],
-                    firstLast: true,
-                    firstText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M13 19L7 12L13 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M16.9998 19L10.9998 12L16.9998 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
-                    lastText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M11 19L17 12L11 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M6.99976 19L12.9998 12L6.99976 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
-                    prevText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M15 5L9 12L15 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
-                    nextText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M9 5L15 12L9 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
-                    labels: {
-                        perPage: '{select}',
-                    },
-                    layout: {
-                        top: '{search}',
-                        bottom: "<div class='flex items-center gap-4'>{info}{select}</div>{pager}",
-                    },
-                });
-            },
+                    ],
+                },
+                perPage: 10,
+                perPageSelect: [10, 20, 30, 50, 100],
+                columns: [{
+                    select: 0,
+                    sort: 'asc',
+                }, ],
+                firstLast: true,
+                firstText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M13 19L7 12L13 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M16.9998 19L10.9998 12L16.9998 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+                lastText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M11 19L17 12L11 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M6.99976 19L12.9998 12L6.99976 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+                prevText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M15 5L9 12L15 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+                nextText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M9 5L15 12L9 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+                labels: {
+                    perPage: '{select}',
+                },
+                layout: {
+                    top: '{search}',
+                    bottom: "<div class='flex items-center gap-4'>{info}{select}</div>{pager}",
+                },
+            });
+        },
 
-            printTable() {
-                this.datatable.print();
-            },
+        printTable() {
+            this.datatable.print();
+        },
 
-            formatDate(date) {
-                if (date) {
-                    const dt = new Date(date);
-                    const month = dt.getMonth() + 1 < 10 ? '0' + (dt.getMonth() + 1) : dt.getMonth() +
-                        1;
-                    const day = dt.getDate() < 10 ? '0' + dt.getDate() : dt.getDate();
-                    return day + '/' + month + '/' + dt.getFullYear();
-                }
-                return '';
-            },
-        }));
-    })
-
-    function insertdata(id){
-        eraseCookie("edit_id");
-        eraseCookie("view_id");
-        window.location = "add_notification.php";
-    }
-
-    async function showAlert(id) {
-        new window.Swal({
-            title: 'Are you sure?',
-            text: "You want be able to revert this!",
-            showCancelButton: true,
-            confirmButtonText: 'Delete',
-            padding: '2em',
-        }).then((result) => {
-            if (result.isConfirmed) {
-                var loc = "notification.php?flg=del&n_id=" + id;
-                window.location = loc;
+        formatDate(date) {
+            if (date) {
+                const dt = new Date(date);
+                const month = dt.getMonth() + 1 < 10 ? '0' + (dt.getMonth() + 1) : dt.getMonth() +
+                    1;
+                const day = dt.getDate() < 10 ? '0' + dt.getDate() : dt.getDate();
+                return day + '/' + month + '/' + dt.getFullYear();
             }
-        });
-    }
+            return '';
+        },
+    }));
+})
+
+function insertdata(id) {
+    eraseCookie("edit_id");
+    eraseCookie("view_id");
+    window.location = "add_notification.php";
+}
+
+async function showAlert(id) {
+    new window.Swal({
+        title: 'Are you sure?',
+        text: "You want be able to revert this!",
+        showCancelButton: true,
+        confirmButtonText: 'Delete',
+        padding: '2em',
+    }).then((result) => {   
+        if (result.isConfirmed) {
+            var loc = "notification.php?flg=del&n_id=" + id;
+            window.location = loc;
+        }
+    });
+}
 </script>
 
 <?php include "footer.php";
