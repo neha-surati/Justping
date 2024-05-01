0af51c368906e6dd2ed62ed61357445f2e681fe6
Neha <nehasurati1605@gmail.com>
2024-05-01 17:00:15 +0530

    added grid in order page

--- order.php
+++ order.php
@@ -1,216 +1,174 @@
-<?php
-include "header.php";
+<?php 
+    include "header.php";
 
-if (isset($_COOKIE["view_id"])) {
-    $mode = 'view';
-    $viewId = $_COOKIE["view_id"];
-    $stmt = $obj->con1->prepare("SELECT * FROM `customer_reg`where id=?");
-    $stmt->bind_param('i', $viewId);
-    $stmt->execute();
-    $Resp = $stmt->get_result();
-    $data = $Resp->fetch_assoc();
-    $stmt->close();
-}
-
-if (isset($_COOKIE["edit_id"])) {
-    $mode = 'edit';
-    $editId = $_COOKIE["edit_id"];
-    $stmt = $obj->con1->prepare("SELECT * FROM `customer_reg` where id=?");
-    $stmt->bind_param('i', $editId);
-    $stmt->execute();
-    $Resp = $stmt->get_result();
-    $data = $Resp->fetch_assoc();
-    $stmt->close();
-}
-
-if (isset($_REQUEST["save"])) {
-    $firstname = $_REQUEST["name"];
-    $lastname = $_REQUEST["lname"];
-    $contact= $_REQUEST["contact"];
-    $email = $_REQUEST["email"];
-    $user_id= $_REQUEST["uid"];
-    $password = $_REQUEST["password"];
-    $status = isset($_REQUEST["status"])?'Enable':'Disable';
-    $operation = "Added";
-    $user_type="admin";
-  
-
-//  echo "INSERT INTO  `customer_reg`(`name`, `lastname`, `username`, `password`, `email`, `contact`,`status`,`operation`,`type`) VALUES ( '".$firstname."', '".$lastname."',  '".$user_id."', '".$password."' , '".$email."', '".$contact."',  '".$status."', '".$operation."', '".$user_type."')";
-    
-        try {
-            $stmt = $obj->con1->prepare(
-                "INSERT INTO `customer_reg`(`firstname`, `lastname`, `username`,`password`, `email`, `contact`,`status`,`operation`,`type`) VALUES (?,?,?,?,?,?,?,?,?)"
-            );
-            $stmt->bind_param("sssssssss", $firstname, $lastname,$user_id, $password , $email,  $contact,  $status, $operation, $user_type);
-            $Resp = $stmt->execute();
-            if (!$Resp) {
-                throw new Exception(
-                    "Problem in adding! " . strtok($obj->con1->error, "(")
-                );
-            }
-            $stmt->close();
-        } catch (\Exception $e) {
-            setcookie("sql_error", urlencode($e->getMessage()), time() + 3600, "/");
-        }
-
-        if ($Resp) {
-            setcookie("msg", "data", time() + 3600, "/");
-              header("location:customer_reg.php");
-        } else {
-            setcookie("msg", "fail", time() + 3600, "/");
-             header("location:customer_reg.php");
-        }
-  
-}
-
-if (isset($_REQUEST["update"])) {
-    $firstname = $_REQUEST["name"];
-    $lastname = $_REQUEST["lname"];
-    $contact= $_REQUEST["contact"];
-    $email = $_REQUEST["email"];
-    $user_id= $_REQUEST["uid"];
-    $password = $_REQUEST["password"];
-    $status = isset($_REQUEST["status"])?'Enable':'Disable';
-    $operation = "update";
-    $user_type="admin";
-    $editId = $_COOKIE["edit_id"];
+	if (isset($_REQUEST["flg"]) && $_REQUEST["flg"] == "del") {
+	$id = $_REQUEST['id'];
+   
 
     try {
-        $stmt = $obj->con1->prepare(
-            "UPDATE   `customer_reg` SET `firstname`=?, `lastname`=?, `username`=?, `password`=?, `email`=?, `contact`=?, `status`=?,`operation`=?,`type`=? WHERE `id`=?"
-        );
-        // echo  "UPDATE  `vendor_reg` SET`name`= '".$firstname."', `lname`='".$lastname."', `username`=  '".$user_id."', `password`= '".$password."', `email`='".$email."', `business_name`= '".$business_name."', `city`='".$city."', `area`='".$area."', `address`='".$address."',`contact_person`= '".$contact_person."', `contact`= '".$contact."', `rating`='".$rating."',`stats`= '".$status."',`added_by`='".$id."',`operation`='".$operation."',`user_type`='".$user_type."',`isopen`='".$isopen."' WHERE `id`='".$editId."'";
-        $stmt->bind_param("sssssssssi", $firstname, $lastname,$user_id, $password , $email,  $contact,  $status, $operation, $user_type,$editId);
-        $Resp = $stmt->execute();
+        $stmt_del = $obj->con1->prepare("DELETE FROM `ordr` WHERE id=?");
+        $stmt_del->bind_param("i",$id);
+        $Resp = $stmt_del->execute();
         if (!$Resp) {
-            throw new Exception(
-                "Problem in adding! " . strtok($obj->con1->error, "(")
-            );
-        }
-        $stmt->close();
-    } catch (\Exception $e) {
-        setcookie("sql_error", urlencode($e->getMessage()), time() + 3600, "/");
-    }
+           throw new Exception("Problem in deleting! ". strtok($obj->con1-> error,  '('));
+       }
+       $stmt_del->close();
+   } catch (\Exception $e) {
+      setcookie("sql_error", urlencode($e->getMessage()), time() + 3600, "/");
+  }
 
-    if ($Resp) {
-        setcookie("msg", "update", time() + 3600, "/");
-        header("location:customer_reg.php");
-    } else {
-        setcookie("msg", "fail", time() + 3600, "/");
-        header("location:customer_reg.php");
-    }
+  if ($Resp) {
+  setcookie("msg", "data_del", time() + 3600, "/");
 }
-
+header("location:customer_addresses.php");
+}
 ?>
+<div class='p-6 animate__animated' x-data='pagination'>
+    <h1 class="dark:text-white-dar  pb-8 text-3xl font-bold">Order</h1>
+    <div class="panel mt-6 flex items-center  justify-between relative">
 
-<div class='p-6'>
-    <div class='flex items-center mb-3 gap-6'>
-        <span class="cursor-pointer">
-            <a href="javascript:go_back()" class="text-3xl text-black dark:text-white">
-                <i class="ri-arrow-left-line"></i>
-            </a>
-        </span>
-        <h1 class="dark:text-white-dar text-2xl font-bold">Order -
-            <?php echo (isset($mode)) ? (($mode == 'view') ? 'View' : 'Edit') : 'Add' ?>
-        </h1>
+        <button type="button" class="p-2 btn btn-primary m-1 add-btn" onclick="javascript:insertdata()">
+            <i class="ri-add-line mr-1"></i> Add Order</button>
+
+        <table id="myTable" class="table-hover whitespace-nowrap w-full"></table>
     </div>
-    <div class="panel mt-6">
-        <div class="mb-5">
-            <form class="space-y-5" method="post">
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
-                    <div>
-                        <label for="name">First Name</label>
-                        <input id="name" name="name" type="text" class="form-input"
-                            placeholder="Enter your first name"
-                            value="<?php echo (isset($mode)) ? $data['firstname'] : '' ?>" required
-                            <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> />
-                    </div>
-                    <div>
-                        <label for="lname">Last Name</label>
-                        <input id="lname" name="lname" type="text" class="form-input"
-                            placeholder="Enter your last name"
-                            value="<?php echo (isset($mode)) ? $data['lastname'] : '' ?>" required
-                            <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> />
-                    </div>
-                </div>
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
-                <div>
-                        <label for="phone_no">Phone Number</label>
-                        <div>
-                            <div class="flex">
-                                <div
-                                    class="bg-[#eee] flex justify-center items-center ltr:rounded-l-md rtl:rounded-r-md px-3 font-semibold border ltr:border-r-0 rtl:border-l-0 border-[#e0e6ed] dark:border-[#17263c] dark:bg-[#1b2e4b]">
-                                    +91</div>
-                                <input id="contact" name="contact" type="text" placeholder="Enter  Phone Number"
-                                    class="form-input ltr:rounded-l-none rtl:rounded-r-none"
-                                    onkeypress="return event.charCode >= 48 && event.charCode <= 57" maxlength="10"
-                                    value="<?php echo (isset($mode)) ? $data['contact'] : '' ?>" required
-                                    <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> />
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <label for="email">Email</label>
-                        <input id="email" name="email" type="email" class="form-input" placeholder="Enter your Email"
-                            value="<?php echo (isset($mode)) ? $data['email'] : '' ?>" required
-                            <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> />
-                    </div>
-                </div>
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
-                    <div>
-                        <label for="gridUID">User Name</label>
-                        <input type="text" placeholder="Enter your Userid" name="uid" id="uid" class="form-input"
-                            value="<?php echo (isset($mode)) ? $data['username'] : '' ?>" required
-                            <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> />
-                    </div>
-                    <div>
-                        <label for="gridpass">Password</label>
-                        <input type="password" placeholder="Enter Password" name="password" class="form-input"
-                            pattern=".{8,}" title="Password should be at least 8 characters long"
-                            value="<?php echo (isset($mode)) ? $data['password'] : '' ?>"
-                            <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?> required />
-                    </div>
-                </div>
-                <div class="mb-4">
-                    <label for="custom_switch_checkbox1">Status</label>
-                    <label class="w-12 h-6 relative">
-                        <input type="checkbox"
-                            class="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer" id="status"
-                            <?php echo isset($mode) && $data['status'] == 'Enable' ? 'checked' : '' ?>
-                            <?php echo (isset($mode) && $mode == 'view') ? 'Disabled' : '' ?> name="status">
-                        <span
-                            class="bg-[#ebedf2] dark:bg-dark block h-full rounded-full before:absolute before:left-1 before:bg-white dark:before:bg-white-dark dark:peer-checked:before:bg-white before:bottom-1 before:w-4 before:h-4 before:rounded-full peer-checked:before:left-7 peer-checked:bg-primary before:transition-all before:duration-300"></span>
-                    </label>
-                </div>
 
-
-                <div class="relative inline-flex align-middle gap-3 mt-4 ">
-                    <button type="submit" name="<?php echo isset($mode) && $mode == 'edit' ? 'update' : 'save' ?>"
-                        id="save" class="btn btn-success <?php echo isset($mode) && $mode == 'view' ? 'hidden' : '' ?>"
-                        <?php echo isset($mode) ? '' : 'onclick="return checkImage()"' ?>>
-                        <?php echo isset($mode) && $mode == 'edit' ? 'Update' : 'Save' ?>
-                    </button>
-                    <button type="button" class="btn btn-danger" onclick="javascript:go_back()">Close</button>
-                </div>
-            </form>
-        </div>
-    </div>
 </div>
 
-<script type="text/javascript">
-// $(document).ready(function() {
-//     eraseCookie("edit_id");
-//     eraseCookie("view_id");
-// });
-// checkCookies();
+<script>
+checkCookies();
+function getActions(id) {
+    return `<ul class="flex items-center gap-4">
+        <li>
+            <a href="javascript:viewdata(` + id + `);" class='text-xl' x-tooltip="View">
+                <i class="ri-eye-line text-primary"></i>
+            </a>
+        </li>
+        <li>
+            <a href="javascript:editdata(` + id + `);" class='text-xl' x-tooltip="Edit">
+                <i class="ri-pencil-line text text-success"></i>
+            </a>
+        </li>
+        <li>
+            <a href="javascript:showAlert(` + id + `);" class='text-xl' x-tooltip="Delete">
+                <i class="ri-delete-bin-line text-danger"></i>
+            </a>
+        </li>
+    </ul>`
+}
+document.addEventListener('alpine:init', () => {
+    Alpine.data('pagination', () => ({
+        datatable: null,
+        init() {
+            this.datatable = new simpleDatatables.DataTable('#myTable', {
+                data: {
+                    headings: ['Sr.No.', 'Vendor Sr.No','Vendor_own_invoice_no','Status','Order From',
+                        ' Delivery Address',   'Action'
+                    ],
+                    data: [
+                        <?php
+                                $stmt = $obj->con1->prepare(" SELECT * FROM `ordr`");
+                                $stmt->execute();
+                                $Resp = $stmt->get_result();
+                                $i = 1;
+                                while ($row = mysqli_fetch_array($Resp)) { ?>
+                                [
+                            <?php echo $i; ?>,
+                            '<?php echo addslashes ($row["vendor_serial_num"]); ?>',
+                            '<?php echo addslashes ($row["vendor_own_invoice_no"]); ?>',
+                            '<?php echo addslashes($row["stats"]);?>',
+                            '<?php echo addslashes($row["order_from"]);?>',
+                            '<?php echo addslashes($row["delivery_address"]);?>',
+                            getActions(<?php echo $row["id"];?>,
+                                )
+                        ],
+                        <?php $i++;}
+                                    ?>
+                    ],
+                },
+                perPage: 10,
+                perPageSelect: [10, 20, 30, 50, 100],
+                columns: [{
+                    select: 0,
+                    sort: 'asc',
+                }, ],
+                firstLast: true,
+                firstText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M13 19L7 12L13 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M16.9998 19L10.9998 12L16.9998 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+                lastText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M11 19L17 12L11 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M6.99976 19L12.9998 12L6.99976 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+                prevText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M15 5L9 12L15 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+                nextText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M9 5L15 12L9 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
+                labels: {
+                    perPage: '{select}',
+                },
+                layout: {
+                    top: '{search}',
+                    bottom: "<div class='flex items-center gap-4'>{info}{select}</div>{pager}",
+                },
+            });
+        },
 
-function go_back() {
+        // exportTable(eType) {
+        //     var data = {
+        //         type: eType,
+        //         filename: 'table',
+        //         download: true,
+        //     };
+
+        //     if (data.type === 'csv') {
+        //         data.lineDelimiter = '\n';
+        //         data.columnDelimiter = ';';
+        //     }
+        //     this.datatable.export(data);
+        // },
+
+        // printTable() {
+        //     this.datatable.print();
+        // },
+
+        // formatDate(date) {
+        //     if (date) {
+        //         const dt = new Date(date);
+        //         const month = dt.getMonth() + 1 < 10 ? '0' + (dt.getMonth() + 1) : dt.getMonth() +
+        //             1;
+        //         const day = dt.getDate() < 10 ? '0' + dt.getDate() : dt.getDate();
+        //         return day + '/' + month + '/' + dt.getFullYear();
+        //     }
+        //     return '';
+        // },
+    }));
+})
+
+function insertdata(id) {
     eraseCookie("edit_id");
     eraseCookie("view_id");
-    window.location = "customer_reg.php";
+    window.location = "order_detail.php";
 }
 
+function editdata(id) {
+    createCookie("edit_id", id, 1);
+    window.location = "order_detail.php";
+}
+
+function viewdata(id) {
+    createCookie("view_id", id, 1);
+    window.location = "order_detail.php";
+}
+
+async function showAlert(id) {
+    new window.Swal({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        showCancelButton: true,
+        confirmButtonText: 'Delete',
+        padding: '2em',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            var loc = "ordr.php?flg=del&id=" + id ;
+            window.location = loc;
+        }
+    });
+}
 </script>
 <?php
-include "footer.php";
+	include "footer.php";
 ?>
